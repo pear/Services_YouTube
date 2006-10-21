13f4289c08d441f6488500d6a1ecff828bd355e3
Shin Ohno <shin@php.net>
2006-10-21 03:31:27 +0000

    Added Bug#9917 test
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_YouTube/trunk@222148 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/Bug9917.php
@@ -0,0 +1,30 @@
+<?php
+require_once '../YouTube.php';
+require_once 'PHPUnit2/Framework/TestCase.php';
+
+class Bug9917 extends PHPUnit2_Framework_TestCase
+{
+    const DEV_ID = 'E88fqlcDtlM';
+
+    public function __construct($name)
+    {
+        parent::__construct($name);
+    }
+    public function test9917()
+    {
+        try {
+            $user_id = "ganchiku";
+
+            $youtube = new Services_YouTube(self::DEV_ID, array('usesCache' => true));
+
+            $youtube->setDriver('xmlrpc');
+
+            $user = $youtube->getProfile($user_id);
+            $this->assertEquals('Shin', $user->user_profile->first_name);
+            $this->assertEquals('Ohno', $user->user_profile->last_name);
+        } catch (Selenium_Exception $e) {
+            echo $e;
+        }
+    }
+}
+?>
